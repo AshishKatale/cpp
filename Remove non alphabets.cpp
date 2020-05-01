b1ffe981a653e2f85d793549db0a007d3c973c62
AshishK18 <61214878+AshishK18@users.noreply.github.com>
2020-05-01 12:17:39 +0530

    Create Remove non alphabets.cpp

---
+++ Remove non alphabets.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+  //Type your code here.
+  string s;
+  cin>>s;
+  for(int i=0;i<s.length();i++)
+  {
+    if((s[i]>='a' && s[i]<='z') ||(s[i]>='A' && s[i]<='Z'))
+      continue;
+  	s.erase(s.begin()+i);
+    i--;
+  }
+  cout<<s;
+}
