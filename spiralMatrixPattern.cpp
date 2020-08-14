b8df2a66406ccab426881e0ed74c0c86c793987f
Ashish-GitHub-acc <61214878+Ashish-GitHub-acc@users.noreply.github.com>
2020-08-14 09:48:23 +0530

    Update spiralMatrixPattern.cpp

--- spiralMatrixPattern.cpp
+++ spiralMatrixPattern.cpp
@@ -1,8 +1,17 @@
+/*
+Enter length:6
+  1     2     3     4     5     6   
+ 20    21    22    23    24     7   
+ 19    32    33    34    25     8   
+ 18    31    36    35    26     9   
+ 17    30    29    28    27    10   
+ 16    15    14    13    12    11
+*/
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
+    cout << "Enter length:";
     int n;
     cin >> n;
     int A[n][n];
@@ -38,7 +47,7 @@ int main()
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
-            cout <<setw(3) << A[i][j] << "   ";
+            cout << setw(3) << A[i][j] << "   ";
         cout << "\n";
     }
 }
