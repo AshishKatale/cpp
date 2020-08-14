953d2f9b613dd6780a00a227391918fff65f8ab0
Ashish-GitHub-acc <61214878+Ashish-GitHub-acc@users.noreply.github.com>
2020-08-14 09:45:12 +0530

    Update pascalTriangle.cpp

--- pascalTriangle.cpp
+++ pascalTriangle.cpp
@@ -1,3 +1,14 @@
+/*                                     1    
+                                   1       1    
+                               1       2       1    
+                           1       3       3       1    
+                       1       4       6       4       1    
+                   1       5      10      10       5       1    
+               1       6      15      20      15       6       1    
+           1       7      21      35      35      21       7       1    
+       1       8      28      56      70      56      28       8       1    
+   1       9      36      84     126     126      84      36       9       1 
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -16,6 +27,7 @@ unsigned long comb(int n, int r)
 
 int main()
 {
+    cout << "Enter integer:";
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
